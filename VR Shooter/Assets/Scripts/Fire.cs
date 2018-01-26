b4dfc7f3795489b944efed8d741dfb18bce3f81e
Jbakercoe <jbakercoe@gmail.com>
2018-01-25 20:04:17 -0500

    Add Unity Files
    Adding Unity Files

---
+++ VR Shooter/Assets/Scripts/Fire.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour {
+
+    [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] Transform fireLocation;
+    [SerializeField] GameObject bulletPrefab;
+
+    public void ShootFireBall()
+    {
+        GameObject fireball = Instantiate(bulletPrefab, fireLocation.transform.position, fireLocation.transform.rotation) as GameObject;
+        fireball.GetComponent<Rigidbody>().velocity = fireball.transform.forward * bulletSpeed;
+        fireball.GetComponent<ParticleSystem>().Play();
+        Destroy(fireball, 6f);
+    }
+
+}
---
+++ VR Shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+public class Player : Singleton<Player> {
+
+    [SerializeField] Slider healthSlider;
+    [SerializeField] Image red;
+    [SerializeField] GameObject shotgun;
+    [SerializeField] float timeBetweenShots = 1.1f;
+
+    private int maxHealth = 100;
+    private int currentHealth;
+    private bool isPlaying;
+    private bool hasCompletedLevel = false;
+    private bool readyToFire = true;
+    private Animator gunAnim;
+    private AudioSource gunAudio;
+
+    //public bool HasCompletedLevel
+    //{
+    //    //get { return hasCompletedLevel; }
+    //    //set { hasCompletedLevel = value; }
+    //}
+
+    void Awake()
+    {
+        Assert.IsNotNull(healthSlider);
+        Assert.IsNotNull(red);
+        Assert.IsNotNull(shotgun);
+    }
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = maxHealth;
+        gunAnim = shotgun.GetComponent<Animator>();
+        gunAudio = shotgun.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isPlaying)
+        {
+            if (currentHealth <= 0)
+            {
+                isPlaying = false;
+                GameManager.Instance.PlayerHasDied();
+            } else if (Input.GetButtonDown("Fire1") && readyToFire)
+            {
+                gunAnim.SetTrigger("Fire");
+                gunAudio.Play();
+                readyToFire = false;
+                StartCoroutine(WaitForNextShot());
+            }
+        }
+        
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EnemyAttack") && isPlaying)
+        {
+            takeHit();
+        }
+    }
+
+    private void takeHit()
+    {
+        currentHealth -= 5;
+        healthSlider.value = currentHealth;
+        red.GetComponent<Animator>().SetTrigger("Hit");
+    }
+
+    IEnumerator WaitForNextShot()
+    {
+        yield return new WaitForSeconds(timeBetweenShots);
+        readyToFire = true;
+    }
+
+    public void ResetHealth()
+    {
+        isPlaying = true;
+        currentHealth = maxHealth;
+        healthSlider.value = currentHealth;
+    }
+
+}
